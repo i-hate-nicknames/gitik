bb5a3465ec309cc96a4f25c241131569d6ece968
Never M <nevermn@protonmail.com>
2021-01-28 18:33:56 +0200

    Read tree and print entries recursively

--- packages/plumbing/plumbing.go
+++ packages/plumbing/plumbing.go
@@ -38,6 +38,25 @@ func (te treeEntry) String() string {
 	return fmt.Sprintf("%s %s %s", te.otype.String(), te.oid, te.name)
 }
 
+func parseEntry(data []byte) (treeEntry, error) {
+	parts := bytes.Split(data, []byte(" "))
+	if len(parts) != 3 {
+		return treeEntry{}, fmt.Errorf("parseEntry: wrong length (%d), should be 3", len(parts))
+	}
+	var otype ObjectType
+	switch header := string(parts[0]); header {
+	case "blob":
+		otype = TypeBlob
+	case "tree":
+		otype = TypeTree
+	default:
+		return treeEntry{}, fmt.Errorf("parseEntry: wrong entry type %s", header)
+	}
+	oid := storage.OID(parts[1])
+	name := string(parts[2])
+	return treeEntry{name, oid, otype}, nil
+}
+
 func WriteFile(fileName string) (storage.OID, error) {
 	data, err := ioutil.ReadFile(fileName)
 	if err != nil {
@@ -91,7 +110,42 @@ func WriteTree(directory string) (storage.OID, error) {
 }
 
 func ReadTree(oid storage.OID) error {
+	entries, err := readTreeEntries(oid)
+	if err != nil {
+		return err
+	}
+	for _, entry := range entries {
+		fmt.Println(entry)
+	}
 	return nil
+}
+
+func readTreeEntries(oid storage.OID) ([]treeEntry, error) {
+	data, err := storage.GetObject(oid, []byte(TypeTree))
+	if err != nil {
+		return nil, err
+	}
+	var entries []treeEntry
+	entriesRaw := bytes.Split(data, []byte("\n"))
+	for _, eraw := range entriesRaw {
+		entry, err := parseEntry(eraw)
+		if err != nil {
+			return nil, err
+		}
+		switch entry.otype {
+		case TypeBlob:
+			entries = append(entries, entry)
+		case TypeTree:
+			children, err := readTreeEntries(entry.oid)
+			if err != nil {
+				return nil, err
+			}
+			entries = append(entries, children...)
+		default:
+			return nil, fmt.Errorf("readTreeEntries: unknown object type")
+		}
+	}
+	return entries, nil
 }
 
 func isIgnored(path string) bool {
