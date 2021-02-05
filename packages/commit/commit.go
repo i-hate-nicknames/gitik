5e5cdfa91d2374b02a8aec6794c0a38827901258
Never M <nevermn@protonmail.com>
2021-02-05 18:26:43 +0200

    Add commit parent and set it to HEAD

--- packages/commit/commit.go
+++ packages/commit/commit.go
@@ -1,8 +1,11 @@
 package commit
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"path"
 
 	"github.com/i-hate-nicknames/gitik/packages/constants"
@@ -12,11 +15,22 @@ import (
 
 type Commit struct {
 	Tree    storage.OID
+	Parent  storage.OID
 	Message string
 }
 
 func (c Commit) Encode() []byte {
-	return []byte(fmt.Sprintf("tree %s\n\n%s\n", c.Tree, c.Message))
+	var buf bytes.Buffer
+	buf.WriteString(encodePair("tree", string(c.Tree)))
+	if c.Parent != "" {
+		buf.WriteString(encodePair("parent", string(c.Parent)))
+	}
+	buf.WriteString("\n" + c.Message + "\n")
+	return buf.Bytes()
+}
+
+func encodePair(a, b string) string {
+	return fmt.Sprintf("%s %s\n", a, b)
 }
 
 func MakeCommit(message string) (storage.OID, error) {
@@ -25,7 +39,13 @@ func MakeCommit(message string) (storage.OID, error) {
 		return "", err
 	}
 	c := Commit{Tree: oid, Message: message}
-
+	headOID, err := getHead()
+	if err != nil && !errors.Is(err, errNoHead) {
+		return "", err
+	}
+	if err == nil {
+		c.Parent = headOID
+	}
 	commitOID, err := storage.HashObject(c.Encode(), constants.TypeCommit)
 	if err != nil {
 		return "", err
@@ -43,3 +63,24 @@ func setHead(oid storage.OID) error {
 }
 
 var errNoHead = errors.New("head not found or empty")
+
+func getHead() (storage.OID, error) {
+	path := path.Join(constants.GitDir, constants.HeadName)
+	file, err := os.Open(path)
+	defer file.Close()
+	if errors.Is(err, os.ErrNotExist) {
+		return "", errNoHead
+	}
+	if err != nil {
+		return "", err
+	}
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		return "", err
+	}
+	result := storage.OID(data)
+	if result == "" {
+		return "", errNoHead
+	}
+	return result, nil
+}
